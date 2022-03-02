e649b5ead610f8c1f36363835594189f3992e664
Valeriane <valeri018@yahoo.com>
2022-03-03 03:07:51 +0400

    add functions to project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,81 @@
+import React, { useState, useEffect } from "react";
 import logo from "./GitHub-Mark-Light-32px.png";
-import textLogo from "./GitHub_Logo_White.png"
+import textLogo from "./GitHub_Logo_White.png";
+import userLogo from "./GitHub-Mark-32px.png";
+import userIMG from "./Octocat.png";
 import "./App.css";
 
 function App() {
+  const [avatar, setAvatar] = useState("");
+  const [name, setName] = useState("");
+  const [userName, setUsername] = useState("");
+  const [input, setInput] = useState("");
+  const [userLink, setUserLink] = useState("");
+  const [error, setErorr] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/")
+      .then((respond) => respond.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  const setData = ({ avatar_url, name, login, html_url }) => {
+    setAvatar(avatar_url);
+    setName(name);
+    setUsername(login);
+    setUserLink(html_url);
+  };
+
+  const searchUser = (e) => {
+    // e.preventDefault();
+    setInput(e.target.value);
+  };
+
+  const submit = () => {
+    fetch(`https://api.github.com/users/${input}`)
+      .then((respond) => respond.json())
+      .then((data) => {
+        if (data.message) {
+          setErorr(data.message);
+        } else {
+          setData(data);
+          setErorr(null);
+        }
+      });
+  };
+
   return (
     <div>
-      <div class="navBar">
-        <img className="textLogo"src={textLogo} alt="Logo" height="32px"/>
-        <img className="logo"src={logo} alt="Logo"/>
+      <div className="navBar">
+        <img className="textLogo" src={textLogo} alt="Logo" height="32px" />
+        <img className="logo" src={logo} alt="Logo" />
       </div>
-      <form className="UserSearch">
-        <input className="searchInput" placeholder="GitHub User">
-        </input>
-        <button className="searchBtn">Search</button>
-      </form>
-      <card className="userInfo">
-        <img className="userImg" />
-        <span className="fullName">FullName</span>
-        <span className="userName">UserName</span>
-        <span className="email">&#9993; E-mail</span>
-      </card>
+      <div className="searchBar">
+        <form className="UserSearch" onSubmit={submit}>
+          <input
+            className="input"
+            placeholder="GitHub User"
+            onChange={searchUser}
+          ></input>
+          <button className="searchBtn">Search</button>
+        </form>
+        {error ? (
+          <h1>{error}</h1>
+        ) : (
+          <div className="userInfo">
+            <img className="userImg" src={avatar || userIMG} />
+            <span className="fullName">{name}</span>
+            <span className="userName">
+              <img className="userLogo" src={userLogo} />{" "}
+              <a className="userLink" href={userLink}>
+                {userName || "user"}
+              </a>
+            </span>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
